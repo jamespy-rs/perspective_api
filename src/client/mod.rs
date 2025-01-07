4b45d7c4f0150ee66e3ccb679f0d6d63e06cd32d
jamesbt365 <jamesbt365@gmail.com>
2025-01-07 19:45:09 +0000

    how did the author of the original crate forget this?

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,8 +3,9 @@ pub(crate) mod v1alpha1;
 pub use v1alpha1::models::error::AnalyzeCommentError;
 pub use v1alpha1::models::error::AnalyzeCommentErrorResponse;
 pub use v1alpha1::models::error::MAX_LENGTH;
-pub use v1alpha1::models::request::AnalyzeCommentRequest;
+pub use v1alpha1::models::request::*;
 pub use v1alpha1::models::response::AnalyzeCommentResponse;
 pub use v1alpha1::PerspectiveClient;
 
-// This client is versioned by API. When a new version of the Perspective API is released, a new module should be created here.
+// This client is versioned by API. When a new version of the Perspective API is
+// released, a new module should be created here.
--- src/client/v1alpha1/models/request.rs
+++ src/client/v1alpha1/models/request.rs
@@ -1,33 +1,65 @@
 use serde::{Deserialize, Serialize};
 
-/// This models the entire scope of request options for the API, and can be constructed more easily with the request builder in the service. Documentation for the fields is
-/// copied from [the website](https://developers.perspectiveapi.com/s/about-the-api-methods?language=en_US).
+/// This models the entire scope of request options for the API, and can be
+/// constructed more easily with the request builder in the service.
+/// Documentation for the fields is copied from [the website](https://developers.perspectiveapi.com/s/about-the-api-methods?language=en_US).
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AnalyzeCommentRequest {
-    /// (required) The text to score. This is assumed to be utf8 raw text of the text to be checked. Emoji and other non-ascii characters can be included (HTML will probably result in lower performance).
+    /// (required) The text to score. This is assumed to be utf8 raw text of the
+    /// text to be checked. Emoji and other non-ascii characters can be included
+    /// (HTML will probably result in lower performance).
     pub comment: Comment,
-    /// (optional) A list of objects providing the context for comment. The API currently does not make use of this field, but it may influence API responses in the future.
+    /// (optional) A list of objects providing the context for comment. The API
+    /// currently does not make use of this field, but it may influence API
+    /// responses in the future.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub context: Option<Context>,
-    /// (required) A map from attribute name to a configuration object. See the ‘Attributes and Languages’ page for a list of available attribute names. If no configuration options are specified, defaults are used, so the empty object {} is a valid (and common) choice. You can specify multiple attribute names here to get scores from multiple attributes in a single request.
+    /// (required) A map from attribute name to a configuration object. See the
+    /// ‘Attributes and Languages’ page for a list of available attribute names.
+    /// If no configuration options are specified, defaults are used, so the
+    /// empty object {} is a valid (and common) choice. You can specify multiple
+    /// attribute names here to get scores from multiple attributes in a single
+    /// request.
     pub requested_attributes: RequestedAttributes,
-    /// (optional) A list of ISO 631-1 two-letter language codes specifying the language(s) that comment is in (for example, "en", "es", "fr", "de", etc). If unspecified, the API will auto-detect the comment language. If language detection fails, the API returns an error. Note: See currently supported languages on the ‘Attributes and Languages’ page. There is no simple way to use the API across languages with production support and languages with experimental support only.
+    /// (optional) A list of ISO 631-1 two-letter language codes specifying the
+    /// language(s) that comment is in (for example, "en", "es", "fr", "de",
+    /// etc). If unspecified, the API will auto-detect the comment language. If
+    /// language detection fails, the API returns an error. Note: See currently
+    /// supported languages on the ‘Attributes and Languages’ page. There is no
+    /// simple way to use the API across languages with production support and
+    /// languages with experimental support only.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub languages: Option<Vec<String>>,
-    /// (optional) A boolean value that indicates if the request should return spans that describe the scores for each part of the text (currently done at per-sentence level). Defaults to false.
+    /// (optional) A boolean value that indicates if the request should return
+    /// spans that describe the scores for each part of the text (currently done
+    /// at per-sentence level). Defaults to false.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub span_annotations: Option<bool>,
-    /// (optional) Whether the API is permitted to store comment and context from this request. Stored comments will be used for future research and community attribute building purposes to improve the API over time. Defaults to false (request data may be stored). Warning: This should be set to true if data being submitted is private (i.e. not publicly accessible), or if the data submitted contains content written by someone under 13 years old (or the relevant age determined by applicable law in my jurisdiction).
+    /// (optional) Whether the API is permitted to store comment and context
+    /// from this request. Stored comments will be used for future research and
+    /// community attribute building purposes to improve the API over time.
+    /// Defaults to false (request data may be stored). Warning: This should be
+    /// set to true if data being submitted is private (i.e. not publicly
+    /// accessible), or if the data submitted contains content written by
+    /// someone under 13 years old (or the relevant age determined by applicable
+    /// law in my jurisdiction).
     #[serde(skip_serializing_if = "Option::is_none")]
     pub do_not_store: Option<bool>,
     /// (optional) An opaque token that is echoed back in the response.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub client_token: Option<String>,
-    /// (optional) An opaque session ID. This should be set for authorship experiences by the client side so that groups of requests can be grouped together into a session. This should not be used for any user-specific id. This is intended for abuse protection and individual sessions of interaction.
+    /// (optional) An opaque session ID. This should be set for authorship
+    /// experiences by the client side so that groups of requests can be grouped
+    /// together into a session. This should not be used for any user-specific
+    /// id. This is intended for abuse protection and individual sessions of
+    /// interaction.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub session_id: Option<String>,
-    /// (optional) An opaque identifier associating this comment with a particular community within your platform. If set, this field allows us to differentiate comments from different communities, as each community may have different norms.
+    /// (optional) An opaque identifier associating this comment with a
+    /// particular community within your platform. If set, this field allows us
+    /// to differentiate comments from different communities, as each community
+    /// may have different norms.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub community_id: Option<String>,
 }
@@ -62,10 +94,12 @@ pub struct RequestedAttributes {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ScoreOptions {
-    /// (optional) The score type returned for this attribute. Currently, only "PROBABILITY" is supported. Probability scores are in the range [0,1].
+    /// (optional) The score type returned for this attribute. Currently, only
+    /// "PROBABILITY" is supported. Probability scores are in the range [0,1].
     #[serde(skip_serializing_if = "Option::is_none")]
     pub score_type: Option<String>,
-    /// (optional) The API won't return scores that are below this threshold for this attribute. By default, all scores are returned.
+    /// (optional) The API won't return scores that are below this threshold for
+    /// this attribute. By default, all scores are returned.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub score_threshold: Option<f64>,
 }
